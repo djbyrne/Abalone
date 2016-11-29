b997a05ce3147c90deb0806f0541b4827f3205b1
djbyrne <donaljbyrne@me.com>
2016-11-29 11:17:51 +0000

    Abalone V1.01
    Contains skeleton files. Board has been initialized with a basic co-ordinate system.

---
+++ AbaloneGame/src/Board.java
@@ -0,0 +1,128 @@
+// an implementation of the XO board and the game logic
+// imports necessary for this class
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle; 
+import javafx.scene.transform.Translate;
+// class definition for drawing a game board
+class Board extends Pane {
+// constructor for the class 
+	public Board() {
+		// initialise the boards 
+		board = new int[9][9]; 
+		renders = new Cell[9][9]; 
+		
+		for(int i = 0; i < 9; i++)
+		{
+			for(int j = 0; j < 9; j++) { 
+				board[i][j] = EMPTY;
+				renders[i][j] = new Cell(0,10); 
+				
+					//set the position of renders[i][j]
+					//indent every odd row
+					if(j==0 && i<2 || j==0 && i>6 )
+					{
+						//do nothing
+					}
+					else if(j==1 && i<2 || j==1 && i>7 )
+					{
+						//do nothing
+					}
+					else if(j==2 && i<1 || j==2 && i>7 )
+					{
+						//do nothing
+					}
+					else if(j==3 && i<1 || j==3 && i>8 )
+					{
+						//do nothing
+					}
+					else if(j==5 && i<1 || j==5 && i>8 )
+					{
+						//do nothing
+					}
+					else if(j==6 && i<1 || j==6 && i>7 )
+					{
+						//do nothing
+					}
+					else if(j==7 && i<2 || j==7 && i>7 )
+					{
+						//do nothing
+					}
+					else if(j==8 && i<2 || j==8 && i>6 )
+					{
+						//do nothing
+					}
+					else
+					{
+						if(j %2 == 0)
+						{
+							renders[i][j].setLayoutX((i*38.88)+19.44);
+						}
+						else
+						{
+							renders[i][j].setLayoutX(i*(38.88));
+						}
+						
+						renders[i][j].setLayoutY(j*(17));
+						getChildren().addAll(renders[i][j]);
+					}					
+			}				
+		}
+		
+		current_player = BLACK;
+	}
+// we have to override resizing behaviour to make our view appear properly 
+@Override
+public void resize(double width, double height) {
+            // call the superclass method first
+	super.resize(width, height); 
+	cell_width = width / 9.0;
+    cell_height = height / 9.0;
+}
+
+      // public method for resetting the game
+public void resetGame() {
+}
+
+public void assignNeighbours()
+{
+	
+}
+      // public method that tries to place a piece
+public void placePiece(final double x, final double y) {
+	// translate the x, y coordinates into cell indexes
+	int indexx = (int) (x / cell_width); 
+	int indexy = (int) (y/cell_height);
+	
+	
+	
+	if(indexy%2==0)
+	{
+		System.out.println("even line");
+		indexx = (int) (x - 17);
+		indexx = (int) (indexx / cell_width);
+	}
+	else
+	{
+		System.out.println("odd line");
+		
+	}
+	
+	renders[indexx][indexy].setOpacity(.5);
+}
+// private fields of the class
+private int[][] board; // array that holds all pieces
+private Cell[][] renders; // array that holds all the render pieces
+private Rectangle back; // background of the board
+private Line h1, h2, v1, v2; // horizontal and vertical grid lines 
+private double cell_width, cell_height; 
+// width and height of a cell
+// translation of {one, two} cell {width, height}
+private Translate ch_one, ch_two, cw_one, cw_two;
+private int current_player;
+private int rowCounter,cellCounter,maxCounter;
+
+      // constants for the class
+private final int EMPTY = 0; private final int BLACK = 1; private final int WHITE = 2;
+}
---
+++ AbaloneGame/src/Cell.java
@@ -0,0 +1,37 @@
+// class definition for an X  or O piece
+import javafx.scene.Group;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color; 
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
+import javafx.scene.transform.Translate;
+class Cell extends Group {
+// constructor for the class 
+	public Cell(int type,int side) {
+		//create new hexagon
+        hex = new Polygon(new Hexagon(side).getPoints());
+        root = new HBox( hex);
+        getChildren().addAll(root);
+        
+	}
+      // overridden version of the resize method
+@Override
+public void resize(double width, double height) { // call the super class method
+super.resize(width, height); 
+
+}
+      // overridden version of the relocate method
+@Override
+public void relocate(double x, double y) { }
+// private fields of the class
+
+private int type;
+private Translate pos; // translate that set the position of this piece
+private Polygon hex;
+private HBox root;
+
+// lines for drawing the X piece
+      // ellipse for rendering the O piece
+// maintain a copy of the piece type we have
+}
