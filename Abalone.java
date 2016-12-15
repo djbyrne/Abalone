cd6e88d9d3fbf23ca6a84ef2a11b25c169e92424
IanGDoyle <iandoyle86@gmail.com>
2016-12-15 14:47:29 +0000

    Add files via upload

---
+++ Abalone.java
@@ -0,0 +1,30 @@
+// JavaFX example that showcases how to build an entirely custom control // that fits into the JavaFX model.
+// imports
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane; 
+import javafx.stage.Stage;
+// class definition
+public class Abalone { // overridden init method
+// initialise the stack pane and add a custom control to it
+	public Abalone(){
+		sp_mainlayout = new StackPane();
+		cc_custom = new CustomControl(); 
+		sp_mainlayout.getChildren().addAll(cc_custom);
+		
+		
+		Stage gameStage = new Stage();
+		
+			gameStage.setTitle("Abalone"); 
+			gameStage.setScene(new Scene(sp_mainlayout, 380, 170)); 
+			
+			}
+	public StackPane getStackPane(){return sp_mainlayout;}
+      // private fields for this class
+private  StackPane sp_mainlayout; 
+private CustomControl cc_custom;
+
+
+
+
+
+}
---
+++ GameLogic.java
@@ -0,0 +1,107 @@
+
+public class GameLogic {
+	
+	public GameLogic()
+	{
+	
+	}
+	
+	public static CellControl[] getSelected()
+	{
+		CellControl[] x = selected;
+		return x;
+	}
+	
+	public static void setSelected(CellControl c)
+	{
+		
+		//System.out.println(c);
+		if(c.getCell().getType() != 0)
+		{
+			if(selected[0] == null)
+			{
+				selected[0] = c;
+				
+				selected[0].getCell().setSelected();
+				setSelectedNeighbors();
+				//setPlayer(c.getCell().getType());
+				//System.out.println("selected piece at: "+c.getBoardX()+":"+c.getBoardY());
+			}
+			//make sure you can select this cell
+			else if(selected[1] == null && getPlayer() == c.getCell().getType()&&c.isNeighbor(selected[0])!=7)
+			{
+				selected[1] = c;
+				c.getCell().setSelected();
+				if(c.isNeighbor(selected[0])!=7)System.out.println("true"+c.isNeighbor(selected[0]));
+				else System.out.println("false");
+			}
+			else if(selected[2] == null && getPlayer() == c.getCell().getType()
+					&& c.isNeighbor(selected[1]) == selected[1].isNeighbor(selected[0]))
+			{
+				System.out.println("true"+c.isNeighbor(selected[1]));
+				selected[2] = c;
+				c.getCell().setSelected();
+			}
+		}
+		
+			
+	}
+	
+	public static void emptySelected()
+	{
+		selected [0].getCell().setOpacity(1);
+		selected [0] = null;
+		selected [1].getCell().setOpacity(1);
+		selected [1] = null;
+		selected [2].getCell().setOpacity(1);
+		selected [2] = null;
+		System.out.println("emtpied");
+	}
+	
+	public static void setSelectedNeighbors()
+	{
+		selectedNeighbours = selected[0].getNieghbors();
+	}
+	
+	public static CellControl[] getSelectedNeighbors()
+	{
+		return selectedNeighbours;
+	}
+	
+	public static void printArray()
+	{
+		for(int i =0; i<selected.length; i++)
+		{
+			System.out.println(selected[i]);
+		}
+	}
+	
+	public static int getPlayer()
+	{
+		return currentPlayer;
+	}
+	
+	public static void togglePlayer()
+	{
+		
+		if(currentPlayer == 1)currentPlayer = 2;
+		else if(currentPlayer == 2)currentPlayer = 1;
+		Abalone_main.setPlayer(currentPlayer);
+	}
+	
+	
+	
+	//variables
+	private int gameState = 0;
+	private final static int EMPTY = 0; private final static int BLACK = 1; private final static int WHITE = 2;
+	private static CellControl[] selected = {null,null,null};
+	private static CellControl[] selectedNeighbours = new CellControl[6];
+	private static int currentPlayer = BLACK;
+	private static int selDir = 3;
+
+	
+
+}
+
+
+
